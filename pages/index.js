cad469c285a91781b973151d019b54ddd782f45c
Takanobu <61861946+takanobu68@users.noreply.github.com>
2023-04-24 23:49:52 +0900

    トップページのレイアウトを更新

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,28 @@
 import styles from '@/styles/Home.module.css';
 import Layout from '../components/Layout';
 
+import utilStyle from '../styles/utils.module.css';
+import Link from 'next/link';
+
 export default function Home() {
-  return <Layout></Layout>;
+  return (
+    <Layout>
+      <section>
+        <p className={utilStyle.headingMd}>私はエンジニアです。</p>
+      </section>
+      <section>
+        <h2>📝エンジニアのブログ</h2>
+        <div>
+          <article>
+            <Link href={'/'}>
+              <img src='/images/thumbnail01.jpg' />
+            </Link>
+            <Link href={'/'}>SSGとSSRの使い分け</Link>
+            <br />
+            <small>2020年</small>
+          </article>
+        </div>
+      </section>
+    </Layout>
+  );
 }
